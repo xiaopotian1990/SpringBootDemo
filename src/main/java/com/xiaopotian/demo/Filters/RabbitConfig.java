313d127351bb54845ec23f10fb937db234596fad
zoulu <545926080@qq.com>
2018-01-18 01:13:47 +0800

    rabbitmq

---
+++ src/main/java/com/xiaopotian/demo/Filters/RabbitConfig.java
@@ -0,0 +1,13 @@
+package com.xiaopotian.demo.Filters;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+    @Bean
+    public Queue Queue() {
+        return new Queue("hello");
+    }
+}
